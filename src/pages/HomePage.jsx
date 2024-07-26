53881b23aa3c441e09787f094d1bec29d9be984e
arkediagithub <bd@arkedia.in>
2024-07-26 16:16:29 +0530

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import AboutSection from "../components/home-page/AboutSection";
+import AchievementSection from "../components/home-page/AchievementSection";
+import ContactSection from "../components/home-page/ContactSection";
+import FeaturesSection from "../components/home-page/FeaturesSection";
+import HeroSection from "../components/home-page/HeroSection";
+import MetaTags from "../components/MetaTags";
+
+export const HomePage = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
+  // testing push
+
+  return (
+    <>
+      <MetaTags />
+      <HeroSection />
+      <AboutSection />
+      <FeaturesSection />
+      <AchievementSection />
+      <ContactSection />
+    </>
+  );
+};
+
+export default HomePage;
