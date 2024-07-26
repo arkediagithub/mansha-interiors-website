0bfe4f1fef84fa719124671bbc4254e5459032ce
arkediagithub <bd@arkedia.in>
2024-07-26 17:02:29 +0530

    update project photos

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,86 +1,84 @@
 import { randomId } from "@mantine/hooks";
 
 import VastuOfficeImg from "../assets/projects/vastu-office.png";
+import ShivJiImg from "../assets/projects/shiv-ji.png";
+import UdayAgarwalImg from "../assets/projects/uday-agarwal.png";
+import DrAmitImg from "../assets/projects/dr-amit.png";
+import BinitJiImg from "../assets/projects/binit-ji.png";
+import BimalJiImg from "../assets/projects/bimal-ji.png";
+import BikeTriImg from "../assets/projects/bike-and-tri.png";
+import AshokMardaImg from "../assets/projects/ashok-marda.png";
+import AnupJiImg from "../assets/projects/anup-ji.png";
+import SanjayAgarwalImg from "../assets/projects/sanjay-agarwal.png";
 
 export const projects = [
   {
     id: randomId(),
     name: "Vastu Office",
     image: VastuOfficeImg,
-    // image:
-    //   "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design-5.png",
     tags: ["interior design", "office"],
     link: "",
   },
   {
     id: randomId(),
     name: "Shiv Ji",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design-3.png",
+    image: ShivJiImg,
     tags: ["interior design", "living area"],
     link: "",
   },
   {
     id: randomId(),
     name: "Uday Agarwal",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design-4.png",
+    image: UdayAgarwalImg,
     tags: ["interior design", "living area"],
     link: "",
   },
   {
     id: randomId(),
     name: "Sanjay Agarwal",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design-2.png",
+    image: SanjayAgarwalImg,
     tags: ["interior design", "living area"],
     link: "",
   },
   {
     id: randomId(),
     name: "Binit Ji",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design-1.png",
+    image: BinitJiImg,
     tags: ["interior design", "living area"],
     link: "",
   },
   {
     id: randomId(),
     name: "Bike and Tri",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Untitled-design.png",
+    image: BikeTriImg,
     tags: ["interior design", "architecture design", "showroom", "studio"],
     link: "",
   },
   {
     id: randomId(),
     name: "Dr. Amit",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Dr.-Amit.png",
+    image: DrAmitImg,
     tags: ["interior design", "bedroom"],
     link: "",
   },
   {
     id: randomId(),
     name: "Bimal Ji",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/BIMAL-JEE-DOLPHIN-SIGNATURE-Room.jpg",
+    image: BimalJiImg,
     tags: ["interior design", "bedroom"],
     link: "",
   },
   {
     id: randomId(),
     name: "Ashok Marda",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/12/Ashok.png",
+    image: AshokMardaImg,
     tags: ["interior design", "bedroom"],
     link: "",
   },
   {
     id: randomId(),
     name: "Anup Ji",
-    image:
-      "https://www.manshainteriors.com/wp-content/uploads/2022/02/Anup-Ji.png",
+    image: AnupJiImg,
     tags: ["interior design", "bedroom"],
     link: "",
   },
