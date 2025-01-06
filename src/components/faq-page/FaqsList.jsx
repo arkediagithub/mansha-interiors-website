d8b54c049b3980623c212000c3bc2c9a67a6b01c
arkediagithub <bd@arkedia.in>
2025-01-06 17:26:24 +0530

    changes in the faqs page

--- src/components/faq-page/FaqsList.jsx
+++ src/components/faq-page/FaqsList.jsx
@@ -1,19 +1,26 @@
+import Reveal from "../Reveal";
+
 const FaqsList = ({ faqs }) => {
   return (
     <div className="max-w-screen-lg mx-auto">
-      {faqs.map((faq) => (
-        <div
-          key={faq.id}
-          className="collapse collapse-plus bg-base-200 my-4 rounded-none border-l-4 border-l-accent"
-        >
-          <input type="radio" name="my-accordion-3" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            {faq.question}
-          </div>
-          <div className="collapse-content">
-            <p>{faq.answer}</p>
-          </div>
-        </div>
+      {faqs.map((faq, idx) => (
+        <>
+          <Reveal key={faq.id} className="my-3">
+            <div className="collapse collapse-plus bg-base-200 rounded-none border-l-4 border-l-accent border-r-4 border-r-accent">
+              <input
+                type="radio"
+                name="my-faqs"
+                defaultChecked={idx === 0 ? true : false}
+              />
+              <div className="collapse-title text-xl font-medium">
+                {faq.question}
+              </div>
+              <div className="collapse-content">
+                <p>{faq.answer}</p>
+              </div>
+            </div>
+          </Reveal>
+        </>
       ))}
     </div>
   );
