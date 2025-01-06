4b793bbb7b07419d4f5c96ef5c4a147bf91cec09
arkediagithub <bd@arkedia.in>
2025-01-06 13:39:15 +0530

    about page update

--- src/components/about-page/AboutPageSection.jsx
+++ src/components/about-page/AboutPageSection.jsx
@@ -1,60 +1,66 @@
 import { Link } from "react-router-dom";
-import ImageReveal from "../ImageReveal";
-import TextReveal from "../TextReveal";
+import SectionWrapper from "../SectionWrapper";
+import Reveal from "../Reveal";
 
-const AboutPageSection = ({ section, opposite, contact }) => {
+const AboutPageSection = ({
+  section,
+  opposite,
+  contact,
+  id = "about_page_section",
+}) => {
   return (
     <>
-      <section
-        id="aboutSection"
-        className=" px-4 py-6 sm:px-6 sm:py-8 lg:px-8 lg:py-12"
-      >
+      <SectionWrapper id={id}>
         <div className="mx-auto max-w-screen-xl">
           <div
             className={`flex flex-col ${
               opposite ? "md:flex-row-reverse" : "md:flex-row"
             } gap-8 lg:gap-16`}
           >
-            <ImageReveal
-              src={section.image}
-              alt="interior design service image"
-              classNames="h-64 sm:h-96 md:w-1/2"
-            />
+            <Reveal className="w-full md:w-1/2">
+              <img
+                src={section.image}
+                alt="interior design service image"
+                className="h-64 sm:h-96 w-full"
+              />
+            </Reveal>
 
             <div
               id="aboutDetails"
               className="flex flex-col justify-center md:w-1/2"
             >
-              <TextReveal>
-                <div
-                  data-theme="dark"
-                  className="w-[70px] h-[70px] flex items-center justify-center rounded-full mb-4 max-md:hidden bg-accent"
-                >
-                  <section.icon stroke={2} size={"40%"} color="white" />
-                </div>
+              <div
+                data-theme="dark"
+                className="w-[70px] h-[70px] flex items-center justify-center rounded-full mb-4 max-md:hidden bg-accent"
+              >
+                <section.icon stroke={2} size={"40%"} color="white" />
+              </div>
 
+              <Reveal>
                 <h2 className="text-3xl sm:text-4xl md:text-5xl font-dm-serif-display tracking-wider">
                   {section.heading}
                 </h2>
+              </Reveal>
 
+              <Reveal className="my-2">
                 <p className="mt-4 text-gray-600 lg:text-lg">
                   {section.content}
                 </p>
+              </Reveal>
 
-                {contact && (
-                  <Link
-                    to="/contact"
-                    role="button"
-                    className="mt-8 btn btn-neutral md:text-lg"
-                  >
-                    Get in Touch
-                  </Link>
-                )}
-              </TextReveal>
+              {contact && (
+                <Link
+                  to="/contact"
+                  role="button"
+                  className="mt-8 btn btn-neutral w-fit md:text-lg"
+                >
+                  Get in Touch
+                </Link>
+              )}
             </div>
           </div>
         </div>
-      </section>
+      </SectionWrapper>
     </>
   );
 };
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -20,10 +20,25 @@ const AboutUsPage = () => {
         title={aboutUsContent.title}
         subtitle={aboutUsContent.subtitle}
       />
-      <AboutPageSection section={aboutUsContent.sections.welcome} opposite />
-      <AboutPageSection section={aboutUsContent.sections.mission} />
-      <AboutPageSection section={aboutUsContent.sections.commitment} opposite />
-      <AboutPageSection section={aboutUsContent.sections.contact} contact />
+      <AboutPageSection
+        id="about_page_welcome_section"
+        section={aboutUsContent.sections.welcome}
+        opposite
+      />
+      <AboutPageSection
+        id="about_page_mission_section"
+        section={aboutUsContent.sections.mission}
+      />
+      <AboutPageSection
+        id="about_page_commitment_section"
+        section={aboutUsContent.sections.commitment}
+        opposite
+      />
+      <AboutPageSection
+        id="about_page_contact_section"
+        section={aboutUsContent.sections.contact}
+        contact
+      />
     </>
   );
 };
