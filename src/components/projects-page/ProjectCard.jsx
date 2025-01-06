5dcadd5465e84626847e37fb9e7d107c0b5b1da8
arkediagithub <bd@arkedia.in>
2025-01-06 11:22:33 +0530

    add details modal for project cards

--- src/components/projects-page/ProjectCard.jsx
+++ src/components/projects-page/ProjectCard.jsx
@@ -2,22 +2,37 @@ import Reveal from "../Reveal";
 
 const ProjectCard = ({ project }) => {
   return (
-    <div className="relative group h-[300px] lg:h-[400px]">
-      {/* image */}
-      <Reveal className="h-full w-full">
-        <img
-          src={project.image}
-          alt={project.name}
-          className="w-full h-full object-fill"
-        />
-      </Reveal>
-      {/* overlay */}
-      <div className="absolute bg-black opacity-0 inset-0 transition-all group-hover:opacity-50 group-hover:z-10"></div>
-      {/* project name */}
-      <p className="absolute top-5 left-5 opacity-0 transition-all group-hover:opacity-100 group-hover:z-10 text-xl font-bold text-white sm:text-2xl">
-        {project.name}
-      </p>
-    </div>
+    <>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button onClick={() => document.getElementById(project.id).showModal()}>
+        {/* project cards */}
+        <div className="relative group h-[300px] lg:h-[400px]">
+          {/* image */}
+          <Reveal className="h-full w-full">
+            <img
+              src={project.image}
+              alt={project.name}
+              className="w-full h-full object-fill"
+            />
+          </Reveal>
+          {/* overlay */}
+          <div className="absolute bg-black opacity-0 inset-0 transition-all group-hover:opacity-50 group-hover:z-10 pointer-events-none"></div>
+          {/* project name */}
+          <p className="absolute top-5 left-5 opacity-0 transition-all group-hover:opacity-100 group-hover:z-10 text-xl font-bold text-white sm:text-2xl pointer-events-none">
+            {project.name}
+          </p>
+        </div>
+      </button>
+      {/* <dialog id={project.id} className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box w-full max-w-5xl">
+          <h3 className="font-bold text-lg">{project.name}</h3>
+          <p className="py-4 text-xs">Press ESC key or click outside to close</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog> */}
+    </>
   );
 };
 
