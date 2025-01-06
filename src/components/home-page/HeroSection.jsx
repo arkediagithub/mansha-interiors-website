0b6f1308be9ce90749eda32d4dde0196b112bc66
arkediagithub <bd@arkedia.in>
2025-01-06 14:04:34 +0530

    add image sldeshow in home page

--- src/components/home-page/HeroSection.jsx
+++ src/components/home-page/HeroSection.jsx
@@ -4,11 +4,10 @@ import { useRef } from "react";
 
 import heroImg1 from "../../assets/hero.jpg";
 import StatsCounter from "../StatsCounter";
+import ImageSlideshow from "../ImageSlideshow";
 
 const HeroSection = () => {
   const headingRef = useRef(null);
-  const heroImage1Ref = useRef(null);
-  const heroImage2Ref = useRef(null);
   const headingText = "Transform Your Space with Elegance and Style";
 
   const splitText = headingText.split("").map((char, index) => (
@@ -23,38 +22,15 @@ const HeroSection = () => {
       { opacity: 0 },
       { opacity: 1, stagger: 0.03, duration: 1 }
     );
-
-    gsap.from(".imageOverlay", 1, {
-      top: 0,
-      left: 0,
-      width: "100%",
-    });
-
-    const tl = gsap.timeline({ repeat: -1 });
-
-    tl.to(heroImage1Ref.current, {
-      opacity: 1,
-      duration: 2,
-    });
-    tl.to(heroImage1Ref.current, {
-      opacity: 0,
-      duration: 1,
-      delay: 2,
-    });
-    tl.to(
-      heroImage2Ref.current,
-      {
-        opacity: 1,
-        duration: 2,
-      },
-      2
-    );
-    tl.to(heroImage2Ref.current, {
-      opacity: 0,
-      duration: 1,
-      delay: 2,
-    });
   }, []);
+
+  // hero images
+  const images = [
+    heroImg1,
+    "https://images.unsplash.com/photo-1499955085172-a104c9463ece?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
 
   return (
     <>
@@ -72,24 +48,11 @@ const HeroSection = () => {
 
         {/* relative image container */}
         <div className="relative ">
-          {/* hero image */}
-          <div className="w-full md:w-[85%] relative aspect-[16/9] mx-auto overflow-hidden">
-            <img
-              ref={heroImage1Ref}
-              src={heroImg1}
-              alt="hero image"
-              className="heroImage absolute top-0 left-0 h-full w-full object-cover opacity-0"
-            />
-            <img
-              ref={heroImage2Ref}
-              src={
-                "https://plus.unsplash.com/premium_photo-1670360414483-64e6d9ba9038?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              }
-              alt="hero image"
-              className="heroImage absolute top-0 left-0 h-full w-full object-cover opacity-0"
-            />
-            <div className="imageOverlay absolute top-0 left-0 h-full bg-accent"></div>
-          </div>
+          {/* hero image slideshow*/}
+          <ImageSlideshow
+            images={images}
+            className="w-full md:w-[85%] relative aspect-[16/9] mx-auto"
+          />
 
           {/* floating contact card */}
           <div className="w-fit p-4 md:p-8 bg-white border-t-4 border-t-accent absolute shadow-lg animate-bounce-slow top-[80%] sm:top-[10%] right-[1%] sm:right-[3%] z-10 max-sm:hidden">
