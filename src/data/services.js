1e1ef83521515102239be2117d3e3c5861a713aa
arkediagithub <bd@arkedia.in>
2025-01-06 10:43:40 +0530

    update teams photo

--- src/data/services.js
+++ src/data/services.js
@@ -14,7 +14,7 @@ export const services = [
       "https://images.unsplash.com/photo-1705909772719-8d3bf32552be?q=80&w=1718&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     heading: "Office Interior",
     description:
-      "Create a productive and inspiring work environment. We design offices that boost efficiency and reflect your brand&apos;s identity.",
+      "Create a productive and inspiring work environment. We design offices that boost efficiency and reflect your brand's identity.",
     id: randomId(),
   },
   {
