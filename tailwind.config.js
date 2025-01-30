b6773c12dc0a8be236686abad59e31f8cee53c3a
arkediagithub <bd@arkedia.in>
2025-01-30 17:16:26 +0530

    add alegreya font

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ export default {
       fontFamily: {
         "league-spartan-variable": ["League Spartan Variable"],
         "inter-variable": ["Inter Variable"],
+        "alegreya-sans": ["Alegreya Sans"],
       },
     },
   },
