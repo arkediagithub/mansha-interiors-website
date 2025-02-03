00210783f0e8f15f072d386d251690ad9690b2bf
arkediagithub <bd@arkedia.in>
2025-02-03 16:43:48 +0530

    metatags changes

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -1,18 +1,19 @@
 import { Helmet } from "react-helmet-async";
 
-const MetaTags = ({ title = "Mansha Interiors" }) => {
+const MetaTags = ({
+  title = "Mansha Interiors",
+  description = "Explore Mansha Interiors, interior design services in siliguri, expert design solutions.",
+}) => {
   return (
     <Helmet prioritizeSeoTags>
       <title>{title}</title>
       <meta name="title" content={title} />
-      <meta
-        name="description"
-        content="Explore Mansha Interiors, interior design services in siliguri, expert design solutions."
-      />
+      <meta name="description" content={description} />
       <meta
         name="keywords"
         content="mansha interiors, interior design service, siliguri, kolkata, expert design solutions"
       />
+      <meta name="theme-color" content="#d4b055" />
       <meta name="robots" content="index, follow" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="language" content="English"></meta>
