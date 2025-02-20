194b538cb835c586e674880894f134a94977d6d9
arkediagithub <bd@arkedia.in>
2025-02-20 13:20:15 +0530

    add images to architecture page

--- src/pages/ArchitecturePage.jsx
+++ src/pages/ArchitecturePage.jsx
@@ -17,18 +17,18 @@ const ArchitecturePage = () => {
   }, []);
 
   const architectureServiceImages = [
-    "https://placehold.co/600",
-    "https://placehold.co/600",
-    "https://placehold.co/600",
-    "https://placehold.co/600",
-    "https://placehold.co/600",
+    "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739942000/image0_uwuek9.jpg",
+    "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739942000/V1_rev_2_cc3spo.jpg",
+    "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739942000/3_h0upjs.jpg",
+    "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739941998/1_1_bcnxts.jpg",
+    "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739942208/15_11zon_hcc4lt.jpg",
   ];
 
   return (
     <>
       <MetaTags title="Architecture | Mansha Interior" />
       <PageHeader
-        image={"https://placehold.co/1080x720"}
+        image={"https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739877456/WhatsApp_Image_2025-02-15_at_1.29.12_PM_ou0fmo.jpg"}
         title="Designing Tomorrow, Today"
         subtitle="Innovative and sustainable architectural solutions for timeless spaces."
       />
@@ -41,6 +41,7 @@ const ArchitecturePage = () => {
           "Sustainable & Green Architecture (Eco-Friendly & Energy-Efficient Designs)",
           "Urban & Landscape Architecture (Outdoor Spaces & City Planning)",
         ]}
+        image={'https://res.cloudinary.com/dmuqtk9yg/image/upload/v1739877456/WhatsApp_Image_2025-02-15_at_1.29.07_PM_yaf4zs.jpg'}
       />
       {/* <ServiceBeforeAfter
         title={"From Vision to Reality"}
