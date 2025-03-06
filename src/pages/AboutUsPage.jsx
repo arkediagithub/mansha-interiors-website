5367621106bf598cb4d79c45eb152493c43ccb87
arkediagithub <bd@arkedia.in>
2025-03-06 16:10:05 +0530

    about page hero photo update

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -23,7 +23,7 @@ const AboutUsPage = () => {
       <PageHeader
         title="Who We Are"
         image={
-          "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1740205668/DSC07396_11zon_cemsnj.jpg"
+          "https://res.cloudinary.com/dmuqtk9yg/image/upload/v1740205684/DSC07442_11zon_blbpdp.jpg"
         }
       />
 
